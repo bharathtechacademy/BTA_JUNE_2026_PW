b9b1248c6e9058b5e459afe3166764bdf89b4e02
bharathautomation <139088966+bharathtechacademy@users.noreply.github.com>
2026-07-10 05:05:05 +0530

    OOPS

--- automation/Test.js
+++
@@ -1,8 +0,0 @@
-console.log(2+2);
-
-test.describe('Test', () => {
-  test('Test', async ({ page }) => {
-    await page.goto('https://playwright.dev/');
-    await expect(page).toHaveTitle(/Playwright/);
-  });
-});
