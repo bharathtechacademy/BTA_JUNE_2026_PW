2f464a53471f4432a8966fa2a336a0d2094f2945
bharathautomation <139088966+bharathtechacademy@users.noreply.github.com>
2026-07-11 08:26:47 +0530

    OOPS

--- typescript-training/oops/abstraction/abstraction.ts
+++ typescript-training/oops/abstraction/abstraction.ts
@@ -2,23 +2,31 @@
 
 abstract class CommonMethods {
 
-
     //Method: Method is all about a block of code or a collection of statements written together to complete a specific task. 
-
-    // loginIntoApplication(): void {
-    //     console.log("Enter the username. ")
-    //     console.log("Enter the password. ")
-    //     console.log("Click on the login button. ")
-    // }
-
-    // logoutFromApplication(): void {
-    //     console.log("Click on the profile icon. ")
-    //     console.log("Click on the logout button. ")
-    // }
 
     abstract loginIntoApplication(): void ;
 
     abstract logoutFromApplication(): void;
 
+    navigateToHomePage(): void {
+        console.log("Click on the home button. ")
+    }
+
+}
+
+ abstract class Test extends CommonMethods {
+
+    loginIntoApplication(): void {
+        console.log("Enter the username. ")
+        console.log("Enter the password. ")
+        console.log("Click on the login button. ")
+    }
+
+
+    verifyLoginFunctionality(): void {
+
+       this.navigateToHomePage();
+
+    }
 
 }
